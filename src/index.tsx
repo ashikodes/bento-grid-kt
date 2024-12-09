029aa28b225848de4765fa37fc0761d5f8d844a3
Onyeabor Ashikodi <ashikodiemeka@gmail.com>
2024-12-08 21:27:14 -0500

    fix deploy, missing outputs file

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,11 @@ import App from './App';
 import { Amplify } from 'aws-amplify';
 import outputs from './amplify_outputs.json';
 
-Amplify.configure(outputs);
+if (!outputs) {
+  console.error('Amplify outputs file is missing or empty.');
+} else {
+  Amplify.configure(outputs);
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
